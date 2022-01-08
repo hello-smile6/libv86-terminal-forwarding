3b2c34212606dcc99730d004157bd48665a7a14c
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2022-01-07 19:37:56 -0800

    Push working version
    That was easier than I anticipated.

--- client.js
+++ client.js
@@ -3,3 +3,8 @@ var term=new Terminal();
 Terminal.applyAddon(fullscreen);
 term.open(document.body);
 term.toggleFullScreen();
+const b=new Bugout(prompt("Server address"));
+b.on("message",function(address,message) {
+    term.write(message);
+});
+term.on('key', key => b.rpc("key",key,function() {}));
--- server.js
+++ server.js
@@ -10,12 +10,19 @@ const emulator=new V86Starter({
         url: "https://cors-anywhere.9pfs.repl.co/copy.sh/v86/bios/seabios.bin"
     },
     autostart: true,
-    disable_keyboard: true,
+    // disable_keyboard: true,
     screen_container: document.getElementById("screen"),
     networking_relay_url: "wss://relay.widgetry.org/",
 });
 var term=new Terminal();
-Terminal.loadAddon(fullscreen);
+Terminal.applyAddon(fullscreen);
 term.open(document.getElementById("terminal"));
 const b=new Bugout();
-b.on("seen",function() {})
+term.writeln("Address: " + b.address());
+b.on("seen",function(address) {term.writeln("Seen" + address)});
+b.on("rpc",function(address, call, args) {
+    if(call=="key") {
+        emulator.serial0_send(args[0]);
+    }
+});
+emulator.add_listener("serial0-output-char", (char) => b.send(char));
