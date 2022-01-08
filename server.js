a06db0d437dc276655d84ee2a9d4f61ad0b4a97b
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2022-01-07 18:42:02 -0800

    More work
    Almost there

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 const emulator=new V86Starter({
     wasm_path: "https://cors-anywhere.9pfs.repl.co/copy.sh/v86/build/v86.wasm",
+    cdrom: {
+        url: "https://cors-anywhere.9pfs.repl.co/k.copy.sh/linux3.iso"
+    }
 })
