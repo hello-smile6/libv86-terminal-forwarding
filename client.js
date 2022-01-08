d135d6d084437c8b2b8a1ea37b9e506257cf17be
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2022-01-07 20:08:37 -0800

    Add client address support

--- client.js
+++ client.js
@@ -3,7 +3,13 @@ var term=new Terminal();
 Terminal.applyAddon(fullscreen);
 term.open(document.body);
 term.toggleFullScreen();
-const b=new Bugout(prompt("Server address"));
+if(window.location.hash!=="") {
+    var address=prompt("Server address");
+}
+else {
+    var address=window.location.hash;
+}
+const b=new Bugout(address);
 b.on("message",function(address,message) {
     term.write(message);
 });
