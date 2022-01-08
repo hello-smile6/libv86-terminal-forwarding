327a9209bedb904da0bec67e35f7fa52e5ea0f4c
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2022-01-07 18:51:40 -0800

    Backup changes

--- server.js
+++ server.js
@@ -2,5 +2,20 @@ const emulator=new V86Starter({
     wasm_path: "https://cors-anywhere.9pfs.repl.co/copy.sh/v86/build/v86.wasm",
     cdrom: {
         url: "https://cors-anywhere.9pfs.repl.co/k.copy.sh/linux3.iso"
-    }
-})
+    },
+    vga_bios: {
+        url: "https://cors-anywhere.9pfs.repl.co/copy.sh/v86/bios/vgabios.bin"
+    },
+    bios: {
+        url: "https://cors-anywhere.9pfs.repl.co/copy.sh/v86/bios/seabios.bin"
+    },
+    autostart: true,
+    disable_keyboard: true,
+    screen_container: document.getElementById("screen"),
+    networking_relay_url: "wss://relay.widgetry.org/",
+});
+var term=new Terminal();
+Terminal.loadAddon(fullscreen);
+term.open(document.getElementById("terminal"));
+const b=new Bugout();
+b.on("seen",function() {})
